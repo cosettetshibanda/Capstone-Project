4c537c297b33b9532bf9dfc15e8b8b5c905435d8
Cosette Tshibanda <cosettetshibanda@gmail.com>
2024-01-29 00:57:35 -0700

    set up action mailer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,19 @@ class UsersController < ApplicationController
     def create
         user = User.create!(user_params)
         session[:user_id] = user.id 
+        respond_to do |format|
+            if @user.save
+              # Tell the UserMailer to send a welcome email after save
+              UserMailer.with(user: @user).welcome_email.deliver_later
+      
+              format.html { redirect_to(@user, notice: 'User was successfully created.') }
+              format.json { render json: @user, status: :created, location: @user }
+            else
+              format.html { render action: 'new' }
+              format.json { render json: @user.errors, status: :unprocessable_entity }
+            end
+          end
+        end
         render json: user, status: :created
     end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+    default from: 'cosettetshibanda@gmail.com'
+
+  def welcome_email
+    @user = params[:user]
+    @url  = 'http://theVillage.com/signup'
+    mail(to: @user.email, subject: 'Welcome to The Village')
+  end
 end

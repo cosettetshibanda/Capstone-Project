525801b69e75110c8767a31bd7ffa670474dd574
Cosette Tshibanda <cosettetshibanda@gmail.com>
2024-01-29 20:07:36 -0700

    added answer validation

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,13 @@ class Answer < ApplicationRecord
     belongs_to :user
 
     validates :answer, length: {minimum: 1, maximum: 200}
+    validate :user_cannot_answer_own_question
+
+  private
+
+  def user_cannot_answer_own_question
+    if user_id == question.user_id
+      errors.add(:user, "cannot answer their own question")
+    end
+  end
 end

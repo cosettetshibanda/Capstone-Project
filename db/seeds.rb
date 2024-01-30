6c36412cf3e7161960148e233aff378fe5c7be8d
Cosette Tshibanda <cosettetshibanda@gmail.com>
2024-01-29 19:48:06 -0700

    added seed data and fixed icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "Henry", password: "Switch", email: "Switch@gmail.com")
+User.create(username: "Tim", password: "Playstation", email: "Playstaion@gmail.com")
+User.create(username: "Tam", password: "Tam", email: "Tam@yahoo.com")
+
+Topic.create(topic: "Mixed kids")
+Topic.create(topic: "Toys")
+Topic.create(topic: "Diapers")
+
+Question.create(post: "What products do you use on their hair?", user_id: 4, topic_id: 13)
+Question.create(post: "Where do you buy clothes?", user_id: 5, topic_id: 3)
+Question.create(post: "What age do you start sleep training?", user_id: 2, topic_id: 6)
+
+Answer.create(answer: "Try adding food slowly and give him time to adjust to the textures.", user_id: 4, question_id: 9)
+Answer.create(answer: "You can give her a bit of tough love so she learns how to self soothe.", user_id: 5, question_id: 15)
+Answer.create(answer: "I like advent bottles", user_id: 2, question_id: 12)

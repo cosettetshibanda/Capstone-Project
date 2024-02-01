9ad69a5100788f40e2b9e4267c71c8eee3016130
Cosette Tshibanda <cosettetshibanda@gmail.com>
2024-01-31 19:48:59 -0700

    fixed seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,18 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: "Henry", password: "Switch", email: "Switch@gmail.com")
-User.create(username: "Tim", password: "Playstation", email: "Playstaion@gmail.com")
-User.create(username: "Tam", password: "Tam", email: "Tam@yahoo.com")
+# User.create(username: "Henry", password: "Switch", email: "Switch@gmail.com")
+# User.create(username: "Tim", password: "Playstation", email: "Playstaion@gmail.com")
+# User.create(username: "Tam", password: "Tam", email: "Tam@yahoo.com")
 
-Topic.create(topic: "Mixed kids")
-Topic.create(topic: "Toys")
-Topic.create(topic: "Diapers")
+# Topic.create(topic: "Mixed kids")
+# Topic.create(topic: "Toys")
+# Topic.create(topic: "Diapers")
 
-Question.create(post: "What products do you use on their hair?", user_id: 4, topic_id: 13)
-Question.create(post: "Where do you buy clothes?", user_id: 5, topic_id: 3)
-Question.create(post: "What age do you start sleep training?", user_id: 2, topic_id: 6)
+# Question.create(post: "What products do you use on their hair?", user_id: 4, topic_id: 13)
+# Question.create(post: "Where do you buy clothes?", user_id: 5, topic_id: 3)
+# Question.create(post: "What age do you start sleep training?", user_id: 2, topic_id: 6)
 
-Answer.create(answer: "Try adding food slowly and give him time to adjust to the textures.", user_id: 4, question_id: 9)
-Answer.create(answer: "You can give her a bit of tough love so she learns how to self soothe.", user_id: 5, question_id: 15)
-Answer.create(answer: "I like advent bottles", user_id: 2, question_id: 12)
+# Answer.create(answer: "Try adding food slowly and give him time to adjust to the textures.", user_id: 4, question_id: 9)
+# Answer.create(answer: "You can give her a bit of tough love so she learns how to self soothe.", user_id: 5, question_id: 15)
+# Answer.create(answer: "I like advent bottles", user_id: 2, question_id: 12)
+
+Henry = User.create(username: "Henry", password: "Switch", email: "Switch@gmail.com")
+Tim = User.create(username: "Tim", password: "Playstation", email: "Playstation@gmail.com")
+Tam = User.create(username: "Tam", password: "Tam", email: "Tam@yahoo.com")
+
+mixed_kids = Topic.create(topic: "Mixed kids")
+toys = Topic.create(topic: "Toys")
+diapers = Topic.create(topic: "Diapers")
+
+hair_questions = mixed_kids.questions.create(post: "What products do you use on their hair?", user: Henry)
+clothes_question = toys.questions.create(post: "Where do you buy clothes?", user: Tim)
+sleep_training_question = diapers.questions.create(post: "What age do you start sleep training?", user: Tam)
+
+hair_questions.answers.create(answer: "Try adding food slowly and give him time to adjust to the textures.", user: Henry)
+clothes_questions.answers.create(answer: "You can give her a bit of tough love so she learns how to self soothe.", user: Tim)
+sleep_training_questions.answers.create(answer: "I like advent bottles", user: Tam)
